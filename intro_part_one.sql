a4610ef918294a7db9105a498d7a6cfbc18280e0
J.T. Kaufman <jtkaufman737@gmail.com>
2020-04-16 09:25:03 -0400

    Update before first session

--- intro_part_one.sql
+++ intro_part_one.sql
@@ -308,4 +308,80 @@ SELECT * FROM candidates;
 -- can have catastrophic effects. You also have to have special privileges usually to shut down someone elses transaction so it isn't like someone else can come along and fix it if you leave a transaction open and go out for lunch, which
 -- is a real story that happened to another entry level developer at my first job. Its a good practice to check for open transactions when you are at a stopping point of your database work 
 
+/* 
+1.6 Keys and Constraints 
 
+Like we started talking about in our slide, primary keys are the way we keep column values unique. Usually there is one field 
+that is the most unique thing about the record and the least likely to have duplication. There are two main ways to declare a primary 
+key. Lets take a look at those ways that by making a new table
+
+*/ 
+
+-- When figuring out what field is a good key candidate, you can pretty much use common sense. Just look at what fields could be 
+-- repeated, and which wouldn't be. With this table, what do you think the most unique field is? 
+
+CREATE TABLE jobs(
+  id uuid DEFAULT uuid_generate_v4(),
+  title VARCHAR(50),
+  department VARCHAR(100),
+  city VARCHAR(50), 
+  state VARCHAR(2), 
+);  
+
+-- If you said the ID, you are right. UUIDs are extremely unique and that's why they make great ID fields. Lets look at the two ways 
+-- we could make the primary key. Number one is during table creation: 
+
+CREATE TABLE jobs(
+  id uuid DEFAULT uuid_generate_v4(),
+  title VARCHAR(50),
+  department VARCHAR(100),
+  city VARCHAR(50), 
+  state VARCHAR(2), 
+  PRIMARY KEY(id)
+);
+
+-- Lets add a little data to take this further 
+INSERT INTO jobs(title, department, city, state) VALUES('Software Engineer','SaaS Engineering', 'Durham', 'NC');
+INSERT INTO jobs(title, department, city, state) VALUES('Software Developer','SaaS Engineering', 'Durham', 'NC');
+INSERT INTO jobs(title, department, city, state) VALUES('Implementation Engineer','Marketing', 'Durham', 'NC');
+INSERT INTO jobs(title, department, city, state) VALUES('Web Developer','Client Services', 'Durham', 'NC');
+
+-- For a little more practice with this syntax, lets look at another way to add primary keys on our existing tables. 
+ALTER TABLE candidates ADD CONSTRAINT candidate_id_pk PRIMARY_KEY(id);
+ALTER TABLE jobs ADD CONSTRAINT job_id_pk PRIMARY_KEY(id);
+
+-- Now, what if we wanted a table to track it when our candidates got jobs? 
+CREATE TABLE candidate_role(
+  candidate_id uuid, 
+  job_id uuid
+);
+
+/*
+This would be a great chance to use a FOREIGN KEY, or a reference to another table. In thinking about why we need this, 
+should there every be a situation where a candidate in the candidate_job table is not in the candidate table? Nope, probably not.
+Also every job in the candidate role table should exist in the jobs table and be a valid job. Foreign keys are a way to 
+make sure that happens. So we are going to look at the second method of having constraints to do this, which is adding them
+after a table was created 
+
+This pattern is 
+
+ALTER TABLE <table name> ADD CONSTRAINT <constraint name> <key type>(<key column>) REFERENCES <other table>(<column>);
+
+*/ 
+
+-- First lets practice the syntax for adding primary keys to our other tables 
+ALTER TABLE candidates ADD CONSTRAINT candidate_id_pk PRIMARY KEY(id);
+ALTER TABLE jobs ADD CONSTRAINT job_id_pk PRIMARY KEY(id);
+
+-- Now lets look at the syntax for adding a foreign key 
+ALTER TABLE candidate_role ADD CONSTRAINT candidate_id_fk FOREIGN KEY(candidate_id) REFERENCES candidates(id);
+ALTER TABLE candidate_role ADD CONSTRAINT job_id_fk FOREIGN KEY(job_id) REFERENCES jobs(id);
+
+/*
+One last cool thing. You'll notice in our candidate role table, we don't have a primary key yet. You almost ALWAYS 
+primary key on a table, but in this case the only columns are two foreign keys, and neither of them would be the right choice 
+for a primary key. So the way we could get a primary key in this case is called a compound or composite key. Compound 
+keys are making a primary key of TWO columns that when combined should always be unique.
+*/ 
+
+ALTER TABLE candidate_role ADD CONSTRAINT candidate_job_pk PRIMARY KEY(job_id, candidate_id);
